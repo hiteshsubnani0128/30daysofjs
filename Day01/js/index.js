58661d6c7a6e3b6225605640c826bfd0338780f4
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-04-22 21:01:47 +0530

    Day 01

--- Day01/js/index.js
+++ Day01/js/index.js
@@ -12,33 +12,22 @@ function myFunction(num){
 function myEqual(){
     abc.push([document.getElementById('viewer').innerHTML,'=']);
     let n = document.getElementById('viewer').innerHTML;
-    for(i of abc){
-        for(j of i)
-        if(j=='+'){
-
-        }
-        else if(j=='-'){
-
-        }
-        else if(j=='*'){
-
-        }
-        else if(j=='/'){
-
-        }
-        else if(j=='%'){
-
-        }
-        else{
-
-        }
-
+    for(var i =0;i<abc.length;i++){
+        if(abc[i][1]=='+'){
+            document.getElementById('viewer').innerHTML = parseInt(abc[i][0])+parseInt(abc[i+1][0])
     }
+    else if(abc[i][1]=='+'){
+        document.getElementById('viewer').innerHTML = parseInt(abc[i][0])+parseInt(abc[i+1][0])
+    }
+    
+}
 }
 function mysum(sign){
     abc.push([document.getElementById('viewer').innerHTML,sign]);
     document.getElementById('viewer').innerHTML = 0;
 }
+
+
 function myClear(){
     document.getElementById('viewer').innerHTML = 0;
 }
